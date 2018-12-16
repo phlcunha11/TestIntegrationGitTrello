c3a29c6e6150a1ca33f595e85b62cfb6cc97c24c
phlcunha <phlcunha@hotmail.com>
2018-12-15 23:44:35 -0200

    Teste

--- TestIntegrationGitTrello/TestIntegrationGitTrello/Program.cs
+++ TestIntegrationGitTrello/TestIntegrationGitTrello/Program.cs
@@ -10,7 +10,7 @@ namespace TestIntegrationGitTrello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Teste");
             Console.ReadLine();
         }
     }
